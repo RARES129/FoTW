29c032ef539a3273ecdea9efb3129203757d7ed9
RARES129 <dascaluraresm@gmail.com>
2023-06-21 23:12:49 +0300

    .

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 
 var Routes = require("./src/public/route.js");
-var Server =require("./src/public/server.js");
-var server = http.createServer(Routes, Server);
+var server = http.createServer(Routes);
 
 server.listen(PORT, function () {
   console.log("Server listening on: http://localhost:%s", PORT);
